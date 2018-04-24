baa66235d903f23fcb09b4e1315b8afc5733b4b4
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-04-24 11:18:00 -0400

    rebuild with xmlhttp in inject script

--- extension-src/injectAsyncRenderToolbox.js
+++ extension-src/injectAsyncRenderToolbox.js
@@ -3,6 +3,28 @@
 const s = document.createElement("script");
 s.type = "text/javascript";
 s.async = true;
-// s.src = "https://rawgit.com/sw-yx/async-render-toolbox/addReactAndMonkeyPatch/extension-src/static/js/main.js";
-s.src = "/build/main.js.js";
+s.src = "https://rawgit.com/sw-yx/async-render-toolbox/addReactInjection/extension-src/build/main.js.js";
+// s.src = "/build/main.js.js";
 document.body.appendChild(s);
+
+
+
+console.log('injected js')
+XMLHttpRequest.prototype.realSend = XMLHttpRequest.prototype.send;
+XMLHttpRequest.prototype.send = function (value) {
+  console.log('caught')
+  this.addEventListener(
+    "loadstart",
+    function () {
+      console.log("Loading");
+    },
+    false
+  );
+  // var trap = document.getElementById('trapcounter')
+  // trap.innerHTML = trap.innerHTML + " 3 "
+  window.asyncreacttoolboxqueue = []
+  console.log("********************", window);
+  // if (!window.asyncreacttoolboxqueue) window.asyncreacttoolboxqueue = []
+  // window.asyncreacttoolboxqueue.push(() => this.realSend(value))
+  // console.log('xmlhttp', window.asyncreacttoolboxqueue, this);
+};
---
+++ extension-src/manifest.json.README.md
@@ -0,0 +1,6 @@
+
+# ran into a CSP issue with injecting our js.
+
+Refused to evaluate a string as JavaScript because 'unsafe-eval' is not an allowed source of script in the following Content Security Policy directive: "script-src 'report-sample'
+
+https://github.com/josdejong/mathjs/issues/559
